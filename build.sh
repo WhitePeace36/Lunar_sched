e28958bb755d9992eba64ec0b9cd3b4080aa5b85
Timon <timon2201@gmail.com>
2026-07-13 20:20:29 +0200

    update cmakelists and build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > ./include/vmlinux.h
-cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
 cmake --build build -j
